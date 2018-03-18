5b47fc9fdba193791479cb9391094ec47d1dd4ea
Yekta Tehrani <33529510+YektaTehrani@users.noreply.github.com>
2018-03-18 18:26:37 -0400

    home work4
    progess on homework 4

---
+++ Assets/Scripts/Homework/Homework4.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/*
+Create a cube prefab that has a script attached to it.
+Write a change colour coroutine.
+Create a cube array and change destroy them in play mode using ray casting.
+*/
+public class Homework4 : MonoBehaviour {
+
+
+    //Var
+    public GameObject spherePrefab;
+    IEnumerator createSphereCoroutine;
+    
+    
+
+
+    public Color sphereColor;
+    public Material sphereMaterial;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        createSphereCoroutine = ColorSpheres();
+        StartCoroutine(createSphereCoroutine);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        Debug.Log(Time.time);
+        if (Time.time > 2)
+        {
+            StopCoroutine(createSphereCoroutine);
+            StopAllCoroutines();
+        }
+
+    }
+
+    //Coroutine 
+   // IEnumerator ColorSpheres()
+   // {
+        
+       /* while (true)
+        {
+            Vector3 spherePosition = new Vector3(-5f,8f,6f);
+            Quaternion sphereRotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
+            GameObject newSphere = Instantiate(spherePrefab, spherePosition, sphereRotation);
+            if (Time.time > 2)
+            {
+                StopAllCoroutines();
+            }
+            yield return new WaitForSeconds(1);
+        
+        } */
+    //}
+}
--- Assets/Scripts/School/Session4.cs
+++ Assets/Scripts/School/Session4.cs
@@ -2,28 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Session4 : MonoBehaviour {
+public class Session4 : MonoBehaviour
+{
 
-    //Variable
-    public GameObject coloumnPrefab;
-    IEnumerator createColoumnCoroutin;
+    // Variables
+    public GameObject columnPrefab;
+    IEnumerator createColumnCoroutine;
 
-	// Use this for initialization
-	void Start () {
-
-         
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    //Coroutines
-    IEnumerator DropColoumns()
+    // Use this for initialization
+    void Start()
     {
-       yield return new WaitForSeconds(5);
-        Vector3 columnPosition = new Vector3(Random.Range(-5f,3f), Random.Range(8f,15f), Random.Range(-7f,0f));
-        Quaternion coloumnRotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
-       
+        createColumnCoroutine = DropColumns();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(createColumnCoroutine);
+        Debug.Log(Time.time);
+        if (Time.time > 5)
+        {
+            StopCoroutine(createColumnCoroutine);
+            StopAllCoroutines();
+        }
+    }
+
+    // Coroutines
+    IEnumerator DropColumns()
+    {
+        while (true)
+        {
+            Vector3 columnPosition = new Vector3(Random.Range(-5f, 3f), Random.Range(8f, 15f), Random.Range(-7f, 0f));
+            Quaternion columnRotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
+            GameObject newColumn = Instantiate(columnPrefab, columnPosition, columnRotation);
+            yield return new WaitForSeconds(5);
+        }
     }
 }
